1dbed190abaeabd7b96f746f08936f47c6564657
braxtonhall <braxtonjhall@gmail.com>
2022-10-03 00:35:52 -0700

    Clean up copy and paste code

--- extension/copy.js
+++ extension/copy.js
@@ -1,40 +1,44 @@
+const COLLECTIONS_ID_PREFIX = "collection_u_";
+const SAVE_DATA_KEY = "_save-data";
+const COLLECTIONS_KEY = "___collections_";
+
 const saveData = (parent) =>
     (event) => {
         event.preventDefault();
-        localStorage.copyData = JSON.stringify(getSaveData(parent))
+        localStorage.setItem(SAVE_DATA_KEY, JSON.stringify(getSaveData(parent)));
     };
 
 const loadData = (parent) =>
     (event) => {
         event.preventDefault();
-        const copyData = JSON.parse(localStorage.copyData);
-        insertSaveData(parent, copyData);
-    }
+        const saveData = JSON.parse(localStorage.getItem(SAVE_DATA_KEY));
+        insertSaveData(parent, saveData);
+    };
 
 const getSaveData = (parent) => {
     const elements = getElementsByTags(parent, RELEVANT_TAGS);
-    return elements.reduce((acc, element) => {
-        if (element.id) {
+    return elements.reduce((saveData, element) => {
+        if (element.id && element.type !== 'hidden') {
             const {value, checked} = element;
-            if (element.id.startsWith("collection_u_")) {
-                const collections = acc[COLLECTIONS_KEY] || {};
+            if (element.id.startsWith(COLLECTIONS_ID_PREFIX)) {
+                const collections = saveData[COLLECTIONS_KEY] || {};
                 const [span] = element.parentElement.getElementsByTagName("span");
                 collections[span.textContent] = {value, checked};
-                acc[COLLECTIONS_KEY] = collections;
+                saveData[COLLECTIONS_KEY] = collections;
             } else {
-                acc[element.id] = {value, checked};
+                saveData[element.id] = {value, checked};
             }
         }
-        return acc;
+        return saveData;
     }, {});
 };
 
 const insertSaveData = (parent, saveData) => {
     const elements = getElementsByTags(parent, RELEVANT_TAGS);
     return elements.forEach((element) => {
-        if (element.id) {
+        if (element.id && element.type !== 'hidden') {
             let saveElement = element;
-            if (element.id.startsWith("collection_u_")) {
+            if (element.id.startsWith(COLLECTIONS_ID_PREFIX)) {
                 const [span] = element.parentElement.getElementsByTagName("span");
                 saveElement = saveData[COLLECTIONS_KEY][span.textContent] || element;
             } else {
@@ -47,22 +51,24 @@ const insertSaveData = (parent, saveData) => {
     });
 };
 
+const appendButton = (element, text, onClick) => {
+    const button = document.createElement("button");
+    button.innerHTML = text;
+    button.addEventListener("click", onClick);
+    const td = document.createElement("td");
+    td.appendChild(button);
+    element.appendChild(td);
+};
+
+const appendRow = (editForm) => (table) => {
+    const row = document.createElement("tr");
+    appendButton(row, 'SAVE', saveData(editForm));
+    appendButton(row, 'LOAD', loadData(editForm));
+    const [body] = table.getElementsByTagName("tbody");
+    body.appendChild(row);
+};
+
 window.addEventListener("load", () => {
-
-    const [editWindow] = document.getElementsByClassName("book_bit");
-
-    Array.from(document.getElementsByClassName("book_bitTable")).forEach((element) => {
-        const saveButton = document.createElement("button");
-        const loadButton = document.createElement("button");
-
-        saveButton.innerHTML = 'SAVE';
-        loadButton.innerHTML = 'LOAD';
-
-        saveButton.addEventListener("click", saveData(editWindow));
-        loadButton.addEventListener("click", loadData(editWindow));
-
-        element.appendChild(saveButton);
-        element.appendChild(loadButton);
-
-    });
+    const [editForm] = document.getElementsByClassName("book_bit");
+    Array.from(document.getElementsByClassName("book_bitTable")).forEach(appendRow(editForm));
 });
