b302a017fdaee5fe37ccea0d967bf505babadb69
braxtonhall <braxtonjhall@gmail.com>
2022-10-03 18:06:06 -0700

    #7 Make colour changing more reliable

--- extension/ts/colour.ts
+++ extension/ts/colour.ts
@@ -1,8 +1,19 @@
-window.addEventListener("load", () => {
+const getMasthead = (document: Document): HTMLElement => {
 	const masthead = document.getElementById("masthead");
 	if (masthead) {
-		masthead.style["transition"] = "1s";
-		masthead.style["filter"] = "invert(80%)";
-		masthead.style["-webkit-filter"] = "invert(80%)";
+		return masthead;
+	} else {
+		const frames = Array.from(document.getElementsByTagName("frame")) as HTMLIFrameElement[];
+		return frames
+			.map((frame) => getMasthead(frame.contentWindow.document))
+			.find((masthead) => !!masthead);
+	}
+};
+
+window.addEventListener("load", () => {
+	const masthead = getMasthead(document);
+	if (masthead) {
+		masthead.style["transition"] = "500ms";
+		masthead.style["filter"] = "saturate(0)";
 	}
 });
