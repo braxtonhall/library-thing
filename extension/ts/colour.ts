1746a44e518f414d1f5b8d8a8b93e57d78e2b118
braxtonhall <braxtonjhall@gmail.com>
2022-10-03 18:42:10 -0700

    #7 Also add the VBL logo when plugin is in use

--- extension/ts/colour.ts
+++ extension/ts/colour.ts
@@ -1,19 +1,26 @@
-const getMasthead = (document: Document): HTMLElement => {
-	const masthead = document.getElementById("masthead");
-	if (masthead) {
-		return masthead;
-	} else {
-		const frames = Array.from(document.getElementsByTagName("frame")) as HTMLIFrameElement[];
-		return frames
-			.map((frame) => getMasthead(frame.contentWindow.document))
-			.find((masthead) => !!masthead);
-	}
+const getElement = (document: Document, id: string): HTMLElement =>
+	document.getElementById(id) ??
+	(Array.from(document.getElementsByTagName("frame")) as HTMLIFrameElement[])
+		.map((frame) => getElement(frame.contentWindow.document, id))
+		.find((masthead) => !!masthead);
+
+const editElement = <E extends HTMLElement>(id: string, callback: (element: E) => void) => {
+	const element = getElement(document, id) as E;
+	element && callback(element);
 };
 
+const setLogo = (id: string) =>
+	editElement(id, (logo: HTMLImageElement) =>
+		logo.src = chrome.runtime.getURL("img/icon128.png"));
+
+const setCSS = (id: string, css: Partial<CSSStyleDeclaration>) =>
+	editElement(id, (element) =>
+		Object.entries(css).forEach(([key, value]) => element.style[key] = value));
+
 window.addEventListener("load", () => {
-	const masthead = getMasthead(document);
-	if (masthead) {
-		masthead.style["transition"] = "500ms";
-		masthead.style["filter"] = "saturate(0)";
-	}
+	setCSS("masthead", {transition: "500ms", filter: "saturate(1.5)"});
+
+	const background = `url(${chrome.runtime.getURL("img/icon128.png")}) no-repeat 16px 0`;
+	setCSS("masthead_logo_wordmark", {background});
+	setLogo("masthead_lt_logo");
 });
