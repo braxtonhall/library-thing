4e2b1bad19ffd6a25e79c6f08493954acc8fb8c5
braxtonhall <braxtonjhall@gmail.com>
2022-10-04 22:36:16 -0700

    Document magic selector

--- extension/ts/services/renderFormEmitter.ts
+++ extension/ts/services/renderFormEmitter.ts
@@ -1,6 +1,9 @@
 import {FORM_RENDER_EVENT} from "../constants";
 
 const tryToEmit = () => {
+	// This is relying on the fact that when the edit form is available, the html matches this selector,
+	// and fails to match in all other cases. This IS brittle. If LibraryThing changes
+	// the markup in any way this will just not work
 	if (document.querySelector("#book_editForm > .book_bit")) {
 		window.dispatchEvent(new Event(FORM_RENDER_EVENT));
 	}
