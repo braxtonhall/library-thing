10bbee1e35385a2cb8e07a3e84942720f170caac
braxtonhall <braxtonjhall@gmail.com>
2022-10-15 16:04:00 -0700

    Made worker errors propagate to main thread

--- extension/ts/workers/invoker.ts
+++ extension/ts/workers/invoker.ts
@@ -1,7 +1,21 @@
-import {WorkerKind, WorkerRequest, WorkerResponse} from "./types";
+import {WorkerKind, WorkerRequest, WorkerResponse, WorkerResponseValue, WorkerStatus} from "./types";
 
-const invokeWorker = <K extends WorkerKind>(kind: K, request: WorkerRequest<K>): Promise<WorkerResponse<K>> => {
-	return new Promise((resolve) => chrome.runtime.sendMessage({kind, request}, resolve));
+const invokeWorker = <K extends WorkerKind>(kind: K, request: WorkerRequest<K>): Promise<WorkerResponseValue<K>> => {
+	return new Promise((resolve, reject) =>
+		chrome.runtime.sendMessage(
+			{
+				kind,
+				request,
+			},
+			(response: WorkerResponse<K>) => {
+				if (response.status === WorkerStatus.RESOLVED) {
+					return resolve(response.value);
+				} else {
+					return reject(new Error(response.message));
+				}
+			}
+		)
+	);
 };
 
 export {invokeWorker};
--- extension/ts/workers/types.ts
+++ extension/ts/workers/types.ts
@@ -4,16 +4,29 @@ enum WorkerKind {
 	Get = "get",
 }
 
+enum WorkerStatus {
+	RESOLVED,
+	REJECTED,
+}
+
 type WorkerRequest<Kind extends WorkerKind> = Kind extends WorkerKind.Get ? GetParameters : never;
 
 type TypedWorkerRequest<Kind extends WorkerKind> = {kind: WorkerKind; request: WorkerRequest<Kind>};
 
-type WorkerResponse<Kind extends WorkerKind> = Kind extends WorkerKind.Get ? GetResponse : never;
+type WorkerResponse<Kind extends WorkerKind> = WorkerResponseResolved<Kind> | WorkerResponseRejected;
 
-type Worker<Kind extends WorkerKind> = (request: WorkerRequest<Kind>) => Promise<WorkerResponse<Kind>>;
+type WorkerResponseResolved<Kind extends WorkerKind> = {
+	status: WorkerStatus.RESOLVED;
+	value: WorkerResponseValue<Kind>;
+};
+type WorkerResponseRejected = {status: WorkerStatus.REJECTED; message: string};
+
+type WorkerResponseValue<Kind extends WorkerKind> = Kind extends WorkerKind.Get ? GetResponse : never;
+
+type Worker<Kind extends WorkerKind> = (request: WorkerRequest<Kind>) => Promise<WorkerResponseValue<Kind>>;
 
 type Workers = {
 	[Kind in WorkerKind]: Worker<Kind>;
 };
 
-export {WorkerKind, Workers, WorkerRequest, WorkerResponse, TypedWorkerRequest};
+export {WorkerKind, Workers, WorkerRequest, WorkerResponse, WorkerResponseValue, TypedWorkerRequest, WorkerStatus};
