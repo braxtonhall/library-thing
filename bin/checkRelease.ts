d8dd9b6ac90de6b5ddc1e50d0ab6a8829a5ec0ec
braxtonhall <braxtonjhall@gmail.com>
2022-10-15 16:18:47 -0700

    #76 Check the release when a new tag is pushed

---
+++ bin/checkRelease.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+
+const [tag] = process.argv.slice(2);
+
+if (!/[0-9]+\.[0-9]+\.[0-9]+/.test(tag)) {
+	throw "Tag not formatted correctly. Should be of the form '*.*.*'";
+}
+
+const manifest = JSON.parse(fs.readFileSync("./extension/manifest.json").toString());
+if (manifest.version !== tag) {
+	throw `Tag does not match manifest version '${manifest.version}'`;
+}
+
+const releaseNotes = fs.existsSync(`./docs/releases/${tag}.md`);
+if (!releaseNotes) {
+	throw "No release notes were found for this version!";
+}
