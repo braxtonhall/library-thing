87c660d23b71e38f5999bc5e293831a11b35e605
braxtonhall <braxtonjhall@gmail.com>
2022-10-15 15:20:45 -0700

    #69 Complete Goodreads implementation

--- extension/ts/services/finder/summary/impl/Goodreads.ts
+++ extension/ts/services/finder/summary/impl/Goodreads.ts
@@ -7,9 +7,9 @@ const SEARCH_PATH = "/search?";
 
 const goodreads: SummaryFinder = async ({isbn}: FinderParameters): Promise<SummaryFinderResponse> => {
 	const url = `${BASE_URL}${SEARCH_PATH}${new URLSearchParams({q: isbn}).toString()}`;
-	const div = (await getDocument(url)).getElementsByClassName("BookPageMetadataSection__description").item(0);
-	const summary = div?.textContent ?? "";
-	return summary.replace(/Show more$/, "");
+	const div = (await getDocument(url)).getElementById("description");
+	div?.querySelectorAll("#description > a").forEach((element) => element.remove());
+	return div?.textContent.trim() ?? "";
 };
 
 export {goodreads};
