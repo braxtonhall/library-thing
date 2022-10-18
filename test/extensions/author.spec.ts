9bba139885b8a93e1c7fe41e1b69417debc192a1
braxtonhall <braxtonjhall@gmail.com>
2022-10-17 20:20:32 -0700

    #97 Test pushing tags

--- test/extensions/author.spec.ts
+++ test/extensions/author.spec.ts
@@ -1,4 +1,4 @@
-import {createSyncBookTags} from "../../extension/ts/extensions/author/util";
+import {createPushBookTags, createSyncBookTags} from "../../extension/ts/extensions/author/util";
 import {AuthorRecord} from "../../extension/ts/adapters/author";
 import {BookRecord} from "../../extension/ts/adapters/book";
 import {expect} from "chai";
@@ -9,13 +9,13 @@ const testAuthors = new Map<string, AuthorRecord>([
 ]);
 const getAuthor = async (uuid: string): Promise<AuthorRecord> => testAuthors.get(uuid);
 const setAuthor = (id: string, tags: string[]) => testAuthors.get(id).tags = tags;
+const createBook = (tags: string[], authorIds: string[]): BookRecord => ({id: "book", tags, authorIds});
 
 const saveBook = async () => undefined;
 
 describe("author extension", () => {
 	describe("#syncBook", () => {
 		const updateBook = createSyncBookTags(getAuthor, saveBook);
-		const createBook = (tags: string[], authorIds: string[]): BookRecord => ({id: "book", tags, authorIds});
 
 		it("should leave non-author tags alone", async () => {
 			setAuthor("1", []);
@@ -33,6 +33,11 @@ describe("author extension", () => {
 			setAuthor("1", ["Baz author"]);
 			const result = await updateBook(createBook(["foo", "bar"], ["1"]));
 			expect(result).to.deep.equal(createBook(["foo", "bar", "Baz author"], ["1"]));
+		});
+
+		it("should remove author tags from a book with no authors", async () => {
+			const result = await updateBook(createBook(["foo", "bar", "foo author"], []));
+			expect(result).to.deep.equal(createBook(["foo", "bar"], []));
 		});
 
 		it("should remove an author tag (1 author)", async () => {
@@ -63,6 +68,57 @@ describe("author extension", () => {
 			setAuthor("1", ["Baz author"]);
 			const result = await updateBook(createBook(["foo", "bar", "Foobar author", "C author"], ["1", "2"]));
 			expect(result).to.deep.equal(createBook(["foo", "bar", "Baz author", "B author", "C author"], ["1", "2"]));
+		});
+	});
+
+	describe("#pushBook", () => {
+		const updateBook = createPushBookTags(getAuthor, saveBook);
+
+		it("should leave non-author tags alone", async () => {
+			setAuthor("1", []);
+			const result = await updateBook(createBook(["foo", "bar"], ["1"]));
+			expect(result).to.deep.equal(createBook(["foo", "bar"], ["1"]));
+		});
+
+		it("should not add non-author tags", async () => {
+			setAuthor("1", ["baz"]);
+			const result = await updateBook(createBook(["foo", "bar"], ["1"]));
+			expect(result).to.deep.equal(createBook(["foo", "bar"], ["1"]));
+		});
+
+		it("should add author tags", async () => {
+			setAuthor("1", ["Baz author"]);
+			const result = await updateBook(createBook(["foo", "bar"], ["1"]));
+			expect(result).to.deep.equal(createBook(["foo", "bar", "Baz author"], ["1"]));
+		});
+
+		it("should not remove author tags from a book with no authors", async () => {
+			const result = await updateBook(createBook(["foo", "bar", "foo author"], []));
+			expect(result).to.deep.equal(createBook(["foo", "bar", "foo author"], []));
+		});
+
+		it("should not remove an author tag (1 author)", async () => {
+			setAuthor("1", ["Baz author"]);
+			const result = await updateBook(createBook(["foo", "bar", "Foobar author"], ["1"]));
+			expect(result).to.deep.equal(createBook(["foo", "bar", "Foobar author", "Baz author"], ["1"]));
+		});
+
+		it("should add tags to a book with multiple authors", async () => {
+			setAuthor("1", ["Baz author"]);
+			const result = await updateBook(createBook(["foo", "bar"], ["1", "2"]));
+			expect(result).to.deep.equal(createBook(["foo", "bar", "Baz author", "B author", "C author"], ["1", "2"]));
+		});
+
+		it("should not remove an author tag (multiple authors)", async () => {
+			setAuthor("1", []);
+			const result = await updateBook(createBook(["foo", "bar", "Foobar author"], ["1", "2"]));
+			expect(result).to.deep.equal(createBook(["foo", "bar", "Foobar author", "B author", "C author"], ["1", "2"]));
+		});
+
+		it("should add and remove and retain tags at the same time", async () => {
+			setAuthor("1", ["Baz author"]);
+			const result = await updateBook(createBook(["foo", "bar", "Foobar author", "C author"], ["1", "2"]));
+			expect(result).to.deep.equal(createBook(["foo", "bar", "Foobar author", "C author", "Baz author", "B author"], ["1", "2"]));
 		});
 	});
 });
