3a1b37ee5f095d108cbb3e4178840c16b159dd30
braxtonhall <braxtonjhall@gmail.com>
2022-10-18 10:23:21 -0700

    #87 Append authorization unconditionally

--- extension/ts/services/google/googleFetch.ts
+++ extension/ts/services/google/googleFetch.ts
@@ -2,27 +2,25 @@ import {invokeWorker} from "../../workers/invoker";
 import {WorkerKind} from "../../workers/types";
 import {showToast, ToastType} from "../../ui/toast";
 
-const handleError = (res: Response) => {
+const handleError = (res: Response): Response => {
 	if (!res.ok) {
 		showToast("Could not complete action", ToastType.ERROR);
 	}
+	return res;
 };
 
 type GoogleFetch = <T>(...args: Parameters<typeof fetch>) => Promise<T | null>;
 
-export const googleFetch: GoogleFetch = async (input: URL, init?: RequestInit) => {
-	const url = new URL(input);
-	const params = new URLSearchParams(url.search);
-	url.search = params.toString();
+const appendAuthorization = async (init?: RequestInit): Promise<RequestInit> => {
 	init ??= {};
 	init.headers ??= {};
+	const token = await invokeWorker(WorkerKind.Authorize, null);
+	init = {...init, headers: {...init.headers, Authorization: `Bearer ${token}`}};
+	return init;
+};
 
-	if (init.method !== "GET") {
-		const token = await invokeWorker(WorkerKind.Authorize, null);
-		init = {...init, headers: {...init.headers, Authorization: `Bearer ${token}`}};
-	}
-
-	const res = await fetch(input, init);
-	handleError(res);
+export const googleFetch: GoogleFetch = async (input: URL, init?: RequestInit) => {
+	const authorizedInit = await appendAuthorization(init);
+	const res = await fetch(input, authorizedInit).then(handleError);
 	return res.status === 200 ? res.json() : null;
 };
