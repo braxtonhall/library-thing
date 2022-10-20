afdd8328f9520aca7e4676332c9f1ac271e37311
braxtonhall <braxtonjhall@gmail.com>
2022-10-19 21:11:59 -0700

    #106 Give a proper error on browsers that don't have Google signin built in

--- extension/ts/workers/impl/authorize.ts
+++ extension/ts/workers/impl/authorize.ts
@@ -2,13 +2,13 @@ type AuthorizeParameters = null;
 type AuthorizeResponse = string;
 
 const authorize = (): Promise<AuthorizeResponse> =>
-	new Promise((resolve) =>
-		chrome.identity.getAuthToken(
-			{
-				interactive: true,
-			},
-			resolve
-		)
-	);
+	new Promise((resolve, reject) => {
+		if (chrome?.identity?.getAuthToken) {
+			return chrome.identity.getAuthToken({interactive: true}, resolve);
+		} else {
+			// TODO use chrome.identity.launchWebAuthFlow
+			return reject(new Error("Unsupported browser :("));
+		}
+	});
 
 export {authorize, AuthorizeParameters, AuthorizeResponse};
