f38287a11b410b00b20fb93480b741b8e5147bba
braxtonhall <braxtonjhall@gmail.com>
2022-11-04 17:06:20 -0700

    #121 Use cache the first time you try save tags

--- extension/ts/main/extensions/tags.ts
+++ extension/ts/main/extensions/tags.ts
@@ -8,6 +8,8 @@ import {isAuthorized} from "./author/util/isAuthorized";
 
 declare const SPREADSHEET_ID: string; // set by webpack
 
+type GetTagsOptions = {noCache: boolean};
+
 const createHighlighterComponents = () => {
 	const highlighter = document.createElement("div");
 	highlighter.className = "bookEditInput"; // this comes from LibraryThing. Stealing their css
@@ -54,12 +56,15 @@ const handleViewChange = (tagInput: HTMLTextAreaElement, backdrop: HTMLElement) 
 	return handler();
 };
 
-const getInvalidTags = async (tags: string[]): Promise<string[]> => {
-	const validTags = await getAllTags({noCache: true});
+const getInvalidTags = async (tags: string[], options: GetTagsOptions): Promise<string[]> => {
+	const validTags = await getAllTags(options);
 	return tags.filter((tag) => !validTags.has(tag));
 };
 
-const getUserAcceptance = (invalidTags: string[], saveHandler: () => Promise<boolean>): Promise<boolean> =>
+const getUserAcceptance = (
+	invalidTags: string[],
+	saveHandler: (options: GetTagsOptions) => Promise<boolean>
+): Promise<boolean> =>
 	new Promise<boolean>((resolve) =>
 		createModal({
 			text: "Are you sure? The following tags are not in the Tag Index",
@@ -77,13 +82,13 @@ const getUserAcceptance = (invalidTags: string[], saveHandler: () => Promise<boo
 		})
 	);
 
-const getSecondaryAcceptance = (saveHandler: () => Promise<boolean>): Promise<boolean> => {
+const getSecondaryAcceptance = (saveHandler: (options: GetTagsOptions) => Promise<boolean>): Promise<boolean> => {
 	window.open(`https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID}`);
 	return new Promise<boolean>((resolve) =>
 		createModal({
 			text: "Did you put the new tags in the Tag Index?",
 			buttons: [
-				{text: "Yes!", colour: ModalColour.GREY, onClick: async () => resolve(saveHandler())},
+				{text: "Yes!", colour: ModalColour.GREY, onClick: async () => resolve(saveHandler({noCache: true}))},
 				{text: "Cancel", colour: ModalColour.PURPLE, onClick: async () => resolve(false)},
 			],
 			colour: ModalColour.PURPLE,
@@ -104,22 +109,22 @@ const setTags = (tagInput: HTMLTextAreaElement, tags: Iterable<string>) => {
 	tagInput.dispatchEvent(new Event("change"));
 };
 
-const checkTags = async (tagInput: HTMLTextAreaElement) =>
+const checkTags = async (tagInput: HTMLTextAreaElement, options: GetTagsOptions) =>
 	loaderOverlaid(async () => {
 		setTags(tagInput, await getAncestorTags(getTags(tagInput)));
-		return getInvalidTags(getTags(tagInput));
+		return getInvalidTags(getTags(tagInput), options);
 	});
 
-const handleSave = (tagInput: HTMLTextAreaElement) => {
-	const saveHandler = (): Promise<boolean> =>
-		checkTags(tagInput).then((invalidTags) => {
+const handleSave = (tagInput: HTMLTextAreaElement, options: GetTagsOptions) => {
+	const saveHandler = (options: GetTagsOptions): Promise<boolean> =>
+		checkTags(tagInput, options).then((invalidTags) => {
 			if (invalidTags.length > 0) {
 				return getUserAcceptance(invalidTags, saveHandler);
 			} else {
 				return true;
 			}
 		});
-	return saveHandler;
+	return saveHandler(options);
 };
 
 onFormRender(async (form, forEachElement, onSave) => {
@@ -129,7 +134,7 @@ onFormRender(async (form, forEachElement, onSave) => {
 		if (tagInput && tagInputContainer) {
 			const {highlighter, backdrop} = createHighlighterComponents();
 			tagInputContainer.insertAdjacentElement("afterbegin", backdrop);
-			onSave(handleSave(tagInput));
+			onSave(() => handleSave(tagInput, {noCache: false}));
 			handleViewChange(tagInput, backdrop);
 			return handleInput(tagInput, highlighter);
 		}
