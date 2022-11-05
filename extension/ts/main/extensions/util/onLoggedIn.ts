50fe183a5a7190b911aebcfd9efecd9b35ff32eb
braxtonhall <braxtonjhall@gmail.com>
2022-11-05 14:37:38 -0700

    #138 Remove overlay before calling login callback so user can interact with page faster

--- extension/ts/main/extensions/util/onLoggedIn.ts
+++ extension/ts/main/extensions/util/onLoggedIn.ts
@@ -34,12 +34,9 @@ const removeInjectedButton = (button: HTMLTableCellElement) => {
 const onLoggedIn = async (callback: () => void, container: HTMLElement) => {
 	if (!(await isAuthorized())) {
 		const onClick = () =>
-			loaderOverlaid(() =>
-				authorize(true)
-					.then(() => removeInjectedButton(button))
-					.then(callback)
-					.catch(console.error)
-			);
+			loaderOverlaid(() => authorize(true).then(() => removeInjectedButton(button)))
+				.then(callback)
+				.catch(console.error);
 		const button = createIconButton("Login", "img/login.png", onClick);
 		injectButton(button, container);
 	} else {
