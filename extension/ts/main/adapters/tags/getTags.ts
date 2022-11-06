7223b52f3cd8fc7c200c9334912db170b510379d
braxtonhall <braxtonjhall@gmail.com>
2022-11-06 00:30:33 -0700

    #121 Code cleanup

---
+++ extension/ts/main/adapters/tags/getTags.ts
@@ -0,0 +1,43 @@
+import {makeCache} from "../../util/cache";
+import {TagSearchOptions, TagTree} from "./types";
+import Sheets from "../sheets";
+import {parseTree} from "./parseTags";
+import {incrementColumnBy} from "./util";
+
+declare const SPREADSHEET_ID: string; // Declared in webpack DefinePlugin
+
+const META_TAG_SHEET = "Tag Index Index";
+
+const {asyncCached, setCache} = makeCache<TagTree>();
+
+const rowIsRange = ([, topLeft, width]: string[]): boolean =>
+	topLeft && width && /^[A-Z]+[0-9]+$/.test(topLeft) && /^[0-9]+$/.test(width);
+
+const rowToRange = ([sheet, topLeft, width]: string[]): string => {
+	const left = topLeft.match(/^[A-Z]+/)[0];
+	const right = incrementColumnBy(left, Number(width) - 1);
+	return `${sheet}!${topLeft}:${right}`;
+};
+
+const getTagRanges = async (): Promise<string[]> => {
+	const range = Sheets.createRange(META_TAG_SHEET, "A", "C");
+	const response = await Sheets.readRanges(SPREADSHEET_ID, [range]);
+	return response.valueRanges?.[0].values.filter(rowIsRange).map(rowToRange) ?? [];
+};
+
+const getSheetsTags = async (): Promise<string[][]> => {
+	const ranges = await getTagRanges();
+	const response = await Sheets.readRanges(SPREADSHEET_ID, ranges);
+	return response.valueRanges.flatMap((valueRange) => valueRange.values ?? []);
+};
+
+const getTagTree = async ({noCache}: TagSearchOptions = {noCache: false}) => {
+	const implementation = async () => parseTree(await getSheetsTags());
+	if (noCache) {
+		return implementation().then((tree) => setCache("", tree));
+	} else {
+		return asyncCached("", implementation);
+	}
+};
+
+export {getTagTree};
---
+++ extension/ts/main/adapters/tags/util.ts
@@ -0,0 +1,28 @@
+// TODO this file probably doesn't belong here
+
+const incrementColumn = (column: string): string => {
+	const parts = column.split("");
+	const incParts = (parts: string[]): string[] => {
+		if (parts.length === 0) {
+			return ["A"];
+		} else {
+			const last = parts.pop();
+			if (last === "Z") {
+				return [...incParts(parts), "A"];
+			} else {
+				return [...parts, String.fromCharCode(last.charCodeAt(0) + 1)];
+			}
+		}
+	};
+	return incParts(parts).join();
+};
+
+const incrementColumnBy = (column: string, n: number): string => {
+	if (n <= 0) {
+		return column;
+	} else {
+		return incrementColumnBy(incrementColumn(column), n - 1);
+	}
+};
+
+export {incrementColumnBy};
