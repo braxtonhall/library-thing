9ab19f778643091cc16eccdfd018f947bfb4c468
braxtonhall <braxtonjhall@gmail.com>
2022-11-06 00:19:45 -0700

    #121 Create a better interface specifying where the tags are

--- extension/ts/main/adapters/tags.ts
+++ extension/ts/main/adapters/tags.ts
@@ -6,7 +6,7 @@ type TagTree = Map<string, TagNode>;
 
 declare const SPREADSHEET_ID: string; // Declared in webpack DefinePlugin
 
-const META_SHEET = "META";
+const META_TAG_SHEET = "Tag Index Index";
 
 interface TagSearchOptions {
 	noCache: boolean;
@@ -22,10 +22,44 @@ interface ParserOptions {
 
 const {asyncCached, setCache} = makeCache<TagTree>();
 
+const incrementColumn = (column: string): string => {
+	const parts = column.split("");
+	const incParts = (parts: string[]): string[] => {
+		if (parts.length === 0) {
+			return ["A"];
+		} else {
+			const last = parts.pop();
+			if (last === "Z") {
+				return [...incParts(parts), "A"];
+			} else {
+				return [...parts, String.fromCharCode(last.charCodeAt(0) + 1)];
+			}
+		}
+	};
+	return incParts(parts).join();
+};
+
+const incrementColumnBy = (column: string, n: number): string => {
+	if (n <= 0) {
+		return column;
+	} else {
+		return incrementColumnBy(incrementColumn(column), n - 1);
+	}
+};
+
+const rowIsRange = ([, topLeft, width]: string[]): boolean =>
+	topLeft && width && /^[A-Z]+[0-9]+$/.test(topLeft) && /^[0-9]+$/.test(width);
+
+const rowToRange = ([sheet, topLeft, width]: string[]): string => {
+	const left = topLeft.match(/^[A-Z]+/)[0];
+	const right = incrementColumnBy(left, Number(width) - 1);
+	return `${sheet}!${topLeft}:${right}`;
+};
+
 const getTagRanges = async (): Promise<string[]> => {
-	const range = Sheets.createRange(META_SHEET, "A", "A");
+	const range = Sheets.createRange(META_TAG_SHEET, "A", "C");
 	const response = await Sheets.readRanges(SPREADSHEET_ID, [range]);
-	return response.valueRanges.flatMap((valueRange) => valueRange.values.map((value) => value[0]));
+	return response.valueRanges?.[0].values.filter(rowIsRange).map(rowToRange) ?? [];
 };
 
 const parseRows = ({rows, fromRow, depth, tree, parent}: ParserOptions): number => {
@@ -50,8 +84,9 @@ const parseTree = (rows: string[][]) => {
 };
 
 const getSheetsTags = async (): Promise<string[][]> => {
-	const response = await Sheets.readRanges(SPREADSHEET_ID, await getTagRanges());
-	return response.valueRanges.flatMap((valueRange) => valueRange.values);
+	const ranges = await getTagRanges();
+	const response = await Sheets.readRanges(SPREADSHEET_ID, ranges);
+	return response.valueRanges.flatMap((valueRange) => valueRange.values ?? []);
 };
 
 const getTagTree = async ({noCache}: TagSearchOptions = {noCache: false}) => {
