25e653300872f84bfb5bb10d894b1eafc4c754c1
braxtonhall <braxtonjhall@gmail.com>
2022-11-06 13:52:18 -0800

    #147 Add an actual timeout commonFinder

--- extension/ts/main/services/finder/util/commonFinder.ts
+++ extension/ts/main/services/finder/util/commonFinder.ts
@@ -1,15 +1,35 @@
 import {Finder, FinderParameters} from "../finder";
+import {sleep} from "../../../../common/util/sleep";
+
+const SEARCH_TIMEOUT_MS = 10_000;
+
+const timeout = (): Promise<never> =>
+	sleep(SEARCH_TIMEOUT_MS).then(() => {
+		throw new Error("Timeout reached!");
+	});
+
+const timed =
+	<T>(finder: Finder<T>): Finder<T> =>
+	(parameters: FinderParameters): Promise<T> =>
+		Promise.race([finder(parameters), timeout()]);
+
+const handleRejection =
+	<T>(defaultValue: T) =>
+	(finder: Finder<T>): Finder<T> =>
+	(parameters: FinderParameters) =>
+		finder(parameters).catch((error) => {
+			console.error(error);
+			return defaultValue;
+		});
 
 const commonFinder =
 	<T>(finders: Finder<T>[], defaultValue: T) =>
 	async (parameters: FinderParameters): Promise<T[]> =>
 		Promise.all(
-			finders.map((finder: Finder<T>) =>
-				finder(parameters).catch((error) => {
-					console.error(error);
-					return defaultValue;
-				})
-			)
+			finders
+				.map(timed)
+				.map(handleRejection(defaultValue))
+				.map((finder: Finder<T>) => finder(parameters))
 		);
 
 export {commonFinder};
