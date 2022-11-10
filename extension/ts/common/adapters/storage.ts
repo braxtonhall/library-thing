813ec681dce15cd1c7866752ee8a2a3e2cc5bdf4
braxtonhall <braxtonjhall@gmail.com>
2022-11-09 22:14:15 -0800

    Remove caching from storage so it gets up to date information from other tabs

--- extension/ts/common/adapters/storage.ts
+++ extension/ts/common/adapters/storage.ts
@@ -1,22 +1,15 @@
-import {makeCache} from "../util/cache";
-
 /**
  * This file works across background and foreground threads. Consider migrating all storage to this
  */
 
-const {asyncCached, setCache} = makeCache<any>();
-
 const set = async <T>(key: string, value: T): Promise<T> => {
-	const update = {[key]: setCache(key, value)};
-	await chrome.storage.sync.set(update);
+	await chrome.storage.sync.set({[key]: value});
 	return value;
 };
 
-const get = <T>(key: string): Promise<T> => {
-	return asyncCached(key, async () => {
-		const storage = await chrome.storage.sync.get({[key]: ""});
-		return storage[key];
-	});
+const get = async <T>(key: string): Promise<T> => {
+	const storage = await chrome.storage.sync.get({[key]: ""});
+	return storage[key];
 };
 
 export default {get, set};
