6a06025aa8353b36ced9ac20cd44e65cb8234803
braxtonhall <braxtonjhall@gmail.com>
2023-01-04 13:34:21 +0000

    #221 Remove version check

--- src/ts/content/index.ts
+++ src/ts/content/index.ts
@@ -4,7 +4,6 @@ import "./extensions/banner";
 import "./extensions/copy";
 import "./extensions/pdf";
 import "./extensions/warning";
-import "./extensions/version";
 import "./extensions/summary";
 import "./extensions/author";
 import "./extensions/resize";
