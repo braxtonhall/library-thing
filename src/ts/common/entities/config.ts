671082520f10bb4696812219aa85c6dc472d2e50
braxtonhall <braxtonjhall@gmail.com>
2023-01-04 15:37:24 +0000

    #201 Use storage API for copy/paste

--- src/ts/common/entities/config.ts
+++ src/ts/common/entities/config.ts
@@ -1,4 +1,5 @@
 import storage from "../adapters/storage";
+import {FormData} from "../../content/entities/bookForm";
 
 /**
  * The point is this file is to have storage,
@@ -7,17 +8,24 @@ import storage from "../adapters/storage";
  */
 
 enum ConfigKey {
+	FormData = "form-data",
 	CheckVersionInterval = "check-version-interval",
 }
 
 const ONE_DAY_MS = 86400000;
 
-const configDefaults = {
+type ConfigDefaults = {
+	[ConfigKey.FormData]: FormData;
+	[ConfigKey.CheckVersionInterval]: number;
+};
+
+const configDefaults: ConfigDefaults = {
+	[ConfigKey.FormData]: {},
 	[ConfigKey.CheckVersionInterval]: ONE_DAY_MS,
 };
 
-type ConfigGetter = <K extends ConfigKey>(key: K) => Promise<typeof configDefaults[K]>;
-type ConfigSetter = <K extends ConfigKey>(key: K, value: typeof configDefaults[K]) => Promise<typeof configDefaults[K]>;
+type ConfigGetter = <K extends ConfigKey>(key: K) => Promise<ConfigDefaults[K]>;
+type ConfigSetter = <K extends ConfigKey>(key: K, value: ConfigDefaults[K]) => Promise<ConfigDefaults[K]>;
 
 const get: ConfigGetter = (key) => storage.get(key, configDefaults[key]);
 const set: ConfigSetter = storage.set;
