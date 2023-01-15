9fcd46819c3368050c225d6309eca4d9641bdf20
braxtonhall <braxtonjhall@gmail.com>
2023-01-15 11:01:21 +0100

    #253 Code cleanup

--- src/ts/content/entities/bookForm/data.ts
+++ src/ts/content/entities/bookForm/data.ts
@@ -8,13 +8,24 @@ const COLLECTIONS_KEY = "___collections_";
 const FORM_META_DATA_KEY = "___metadata_";
 const ROLES_INPUT_COUNT_KEY = "___roles-count_";
 
-const TRANSFORMERS = {
-	item_inventory_barcode_1: (incoming: string, existing: string): string => existing,
-	form_comments: (incoming: string, existing: string): string => {
-		// TODO: use existing
-		const lines = incoming.split("\n");
-		const withoutLocation = lines.filter((line) => !/^LOCATION:/i.test(line));
-		return withoutLocation.join("\n");
+/**
+ * This is how specific elements should be transformed as a paste operation occurs
+ * Indexed using the element ID on librarything.com
+ */
+type Transformer<T> = (incoming: T, existing: T) => T;
+const checkedTransformers: {[elementId: string]: Transformer<boolean>} = {};
+const valueTransformers: {[elementId: string]: Transformer<string>} = {
+	item_inventory_barcode_1: (incoming, existing) => existing,
+	form_comments: (incoming, existing) => {
+		if (existing === incoming) {
+			// Don't remove existing data on an accidental paste
+			return existing;
+		} else {
+			return incoming
+				.split("\n")
+				.filter((line) => !/^\s*LOCATION:.+/i.test(line))
+				.join("\n");
+		}
 	},
 };
 
@@ -58,16 +69,20 @@ const getFormData = (_document = document) =>
 		return saveData;
 	}, getFormMetadata(_document));
 
+const transformIncomingData = (element: any, incoming: {value: string; checked: boolean}) => {
+	const transformedValue = valueTransformers[element.id]?.(incoming.value, element.value) ?? incoming.value;
+	const transformedChecked = checkedTransformers[element.id]?.(incoming.checked, element.checked) ?? incoming.checked;
+	return {value: transformedValue, checked: transformedChecked};
+};
+
 const insertFormData = (saveData: FormData) => {
 	ensureRolesInputCount(saveData?.[FORM_META_DATA_KEY]?.[ROLES_INPUT_COUNT_KEY] ?? 0);
 	getFormElements(document).forEach((element: any) => {
 		if (isFormDataElement(element)) {
-			const {value: incomingValue, checked: incomingChecked} = extractSaveDataFor(element, saveData);
-			if (element.value !== incomingValue || element.checked !== incomingChecked) {
-				// We transform AFTER checking equality, because we want to ensure
-				// self-pasting doesn't modify data
-				element.value = TRANSFORMERS[element.id]?.(incomingValue, element.value) ?? incomingValue;
-				element.checked = incomingChecked;
+			const {value, checked} = transformIncomingData(element, extractSaveDataFor(element, saveData));
+			if (element.value !== value || element.checked !== checked) {
+				element.value = value;
+				element.checked = checked;
 				element.dispatchEvent(new Event("change"));
 				ensureVisible(element);
 			}
