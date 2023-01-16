495e805dceee712c82f2bf8733f0792ebb3c7cfa
braxtonhall <braxtonjhall@gmail.com>
2023-01-16 13:31:22 +0100

    #104 Ensure there are no id collisions in the physical description

--- src/ts/content/entities/bookForm/data/uniqueData/physicalDescription/elements/pagination.ts
+++ src/ts/content/entities/bookForm/data/uniqueData/physicalDescription/elements/pagination.ts
@@ -1,9 +1,9 @@
 const pagination = (n: number, showSubtract: boolean, showAdd: boolean) => `
-<fieldset id="fs_u_${n}">
+<fieldset id="fs_u_pag_${n}">
 	<input type="hidden" name="type" value="pages">
 	<input type="hidden" name="deleted" value="">
 	<input type="hidden" id="forcesave_" name="forcesave_pages" value="0">
-	<input type="hidden" name="id" value="fs_u_${n}">
+	<input type="hidden" name="id" value="fs_u_pag_${n}">
 	<input style="width:50px;margin-right:5px;" id="" name="pagecount" class="bookEditInput" value="" onkeyup="handleAutogenChange('');">
 	<select style="width:auto;" name="pagetype" class="bookEditInput">
 		<option value="0" selected="">1,2,3,...</option>
@@ -15,12 +15,12 @@ const pagination = (n: number, showSubtract: boolean, showAdd: boolean) => `
 		<option value="6"></option>
 		<option value="7"></option>
 	</select>
-	<a href="#" id="arbm_fs_u_${n}" style="${
+	<a href="#" id="arbm_fs_u_pag_${n}" style="${
 	showSubtract === false ? "display:none;" : ""
-}" class="rowPlusMinus" onclick="book_deleteUIRow('fs_u_${n}');return false;">–</a>
-	<a href="#" id="arb_fs_u_${n}" style="${
+}" class="rowPlusMinus" onclick="book_deleteUIRow('fs_u_pag_${n}');return false;">–</a>
+	<a href="#" id="arb_fs_u_pag_${n}" style="${
 	showAdd === false ? "display:none;" : ""
-}" class="rowPlusMinus" onclick="book_addUIRow('pages', 'fs_u_${n}');return false;">+</a>
+}" class="rowPlusMinus" onclick="book_addUIRow('pages', 'fs_u_pag_${n}');return false;">+</a>
 </fieldset>
 `;
 
--- src/ts/content/entities/bookForm/data/uniqueData/physicalDescription/elements/weight.ts
+++ src/ts/content/entities/bookForm/data/uniqueData/physicalDescription/elements/weight.ts
@@ -1,20 +1,20 @@
 const weight = (n: number, showSubtract: boolean, showAdd: boolean) => `
-<fieldset id="fs_u_${n}">
+<fieldset id="fs_u_wgt_${n}">
 	<input type="hidden" name="type" value="weight">
 	<input type="hidden" name="deleted" value="">
 	<input type="hidden" id="forcesave_" name="forcesave_weight" value="0">
-	<input type="hidden" name="id" value="fs_u_${n}">
+	<input type="hidden" name="id" value="fs_u_wgt_${n}">
 	<input style="width:50px;margin-right:5px;" id="" name="weight" value="" class="bookEditInput" onkeyup="handleAutogenChange('');">
 	<select style="width:auto;" name="unit" phystag="unit" class="bookEditInput">
 		<option value="0" selected="">pounds</option>
 		<option value="1">kg</option>
 	</select>
-	<a href="#" id="arbm_fs_u_${n}" style="${
+	<a href="#" id="arbm_fs_u_wgt_${n}" style="${
 	showSubtract === false ? "display:none;" : ""
-}" class="rowPlusMinus" onclick="book_deleteUIRow('fs_u_${n}');return false;">–</a>
-	<a href="#" id="arb_fs_u_${n}" style="${
+}" class="rowPlusMinus" onclick="book_deleteUIRow('fs_u_wgt_${n}');return false;">–</a>
+	<a href="#" id="arb_fs_u_wgt_${n}" style="${
 	showAdd === false ? "display:none;" : ""
-}" class="rowPlusMinus" onclick="book_addUIRow('weight', 'fs_u_${n}');return false;">+</a>
+}" class="rowPlusMinus" onclick="book_addUIRow('weight', 'fs_u_wgt_${n}');return false;">+</a>
 </fieldset>
 `;
 
